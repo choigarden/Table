a59bd4d385bbe4970fc3ef0d97d3f871f2692a05
jeongwon <choigarden@nate.com>
2022-08-28 21:41:03 +0900

    아웃렛 변수와 액션 함수 추가하기

--- Table/TableViewController.swift
+++ Table/TableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    @IBOutlet var tvListView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
